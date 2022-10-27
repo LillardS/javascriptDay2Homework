9ccd4d339d9d13d608a14ca7d21d698758ab309a
Seth Lillard <lillardseth@gmail.com>
2022-10-26 21:07:45 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function sum(arg1, arg2) {
+    console.log(arg1 + arg2);
+}
+
+console.log(sum(10, 5));
+
+function difference(arg1, arg2) {
+    console.log(arg1 - arg2);
+}
+
+console.log(difference(10, 5));
+
+function multiply(arg1, arg2) {
+    console.log(arg1 * arg2);
+}
+
+console.log(multiply(10, 5));
+
+function divide(arg1, arg2) {
+    console.log(arg1 / arg2);
+}
+
+console.log(divide(10, 5));
